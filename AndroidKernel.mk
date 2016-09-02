2f3bf04a650d8f07a87e5c59be890018f1590267
Quang Ngô <ngoquang2708@gmail.com>
2016-09-02 18:36:06 +0700

    Update AndroidKernel.mk

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -1,5 +1,4 @@
-KERNEL_OUT ?= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
-KERNEL_MODULES_OUT ?= $(TARGET_OUT_INTERMEDIATES)/lib/modules
+KERNEL_OUT ?= $(OUT)/obj/KERNEL_OBJ
 KERNEL_DEFCONFIG ?= cyanogen_vivalto3gvn_defconfig
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 
@@ -11,18 +10,16 @@ else
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/zImage
 endif
 
+all: $(TARGET_PREBUILT_KERNEL)
+
+kernelheader: $(KERNEL_OUT)
+	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" headers_install
+
 $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
 
 $(KERNEL_CONFIG): $(TARGET_KERNEL_SOURCE)/arch/arm/configs/$(KERNEL_DEFCONFIG)
 	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" $(KERNEL_DEFCONFIG)
 
-$(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
-	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" headers_install
+$(TARGET_PREBUILT_KERNEL): kernelheader $(KERNEL_OUT) $(KERNEL_CONFIG)
 	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" zImage -j4
-	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" modules
-	@-mkdir -p $(KERNEL_MODULES_OUT)
-	@-find $(KERNEL_OUT) -name *.ko | xargs -I{} cp {} $(KERNEL_MODULES_OUT)
-
-kernelheader: $(KERNEL_OUT)
-	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" headers_install
