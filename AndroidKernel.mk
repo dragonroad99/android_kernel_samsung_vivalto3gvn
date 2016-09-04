f65c7bcc78d564d1a007c2ddc59e8b82e255b16e
Quang Ngô <ngoquang2708@gmail.com>
2016-09-04 21:41:01 +0700

    Update AndroidKernel.mk
    1. Use prebuilt ccache
    2. Use arm-eabi toolchain

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -4,6 +4,8 @@ KERNEL_CONFIG := $(KERNEL_OUT)/.config
 
 TARGET_KERNEL_SOURCE ?= $(PWD)
 
+CCACHE ?= $(TARGET_KERNEL_SOURCE)/../../../prebuilts/misc/linux-x86/ccache/ccache
+
 ifeq ($(USES_UNCOMPRESSED_KERNEL),true)
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/Image
 else
@@ -13,13 +15,13 @@ endif
 all: $(TARGET_PREBUILT_KERNEL)
 
 kernelheader: $(KERNEL_OUT)
-	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" headers_install
+	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="$(CCACHE) arm-eabi-" headers_install
 
 $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
 
 $(KERNEL_CONFIG): $(TARGET_KERNEL_SOURCE)/arch/arm/configs/$(KERNEL_DEFCONFIG)
-	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" $(KERNEL_DEFCONFIG)
+	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="$(CCACHE) arm-eabi-" $(KERNEL_DEFCONFIG)
 
 $(TARGET_PREBUILT_KERNEL): kernelheader $(KERNEL_OUT) $(KERNEL_CONFIG)
-	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="ccache arm-linux-androideabi-" zImage -j4
+	$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="$(CCACHE) arm-eabi-" zImage -j4
